0aa5c14710b97040467453fc3545512ebe5c1fe7
SutejNaik <sutejnaik716@gmail.com>
2026-09-18 13:23:04 +0000

    Initial application setup

---
+++ test_app.py
@@ -0,0 +1,14 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello from AWS CI/CD!"
+
+
+if __name__ == "__main__":
+    test_home()
+    print("All tests passed!")
