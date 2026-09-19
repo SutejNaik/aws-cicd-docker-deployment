dd77ce76b61fbd3852e250f2feefb30a66e2d9ce
Sutej S Naik <sutejnaik716@gmail.com>
2026-09-19 20:52:46 +0530

    Update expected response data in test

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello from AWS CI/CD!"
+    assert response.data == b"Hello from AWS CI/CD! version-2"
 
 
 if __name__ == "__main__":
